0b9c998b5dc22e8e9beb49b672616457d37cd1c2
AlexFreitasDev <110000511+alexcaf7@users.noreply.github.com>
2025-01-13 20:24:55 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,74 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const menuToggle = document.getElementById('menu-toggle');
+    const navMenu = document.querySelector('.nav-menu');
+
+    menuToggle.addEventListener('click', function() {
+        navMenu.classList.toggle('active');
+    });
+
+    // Fechar o menu ao clicar em um link
+    const navLinks = document.querySelectorAll('.nav-menu a');
+    navLinks.forEach(link => {
+        link.addEventListener('click', function() {
+            navMenu.classList.remove('active');
+        });
+    });
+});
+
+
+    function openPopup3() {
+        document.getElementById('popup3').style.display = 'flex';
+    }
+
+    function closePopup3() {
+        document.getElementById('popup3').style.display = 'none';
+    }
+
+    function openPopup4() {
+        document.getElementById('popup4').style.display = 'flex';
+    }
+
+    function closePopup4() {
+        document.getElementById('popup4').style.display = 'none';
+    }
+
+    function openPopup5() {
+        document.getElementById('popup5').style.display = 'flex';
+    }
+
+    function closePopup5() {
+        document.getElementById('popup5').style.display = 'none';
+    }
+    function openPopup6() {
+        document.getElementById('popup6').style.display = 'flex';
+    }
+
+    function closePopup6() {
+        document.getElementById('popup6').style.display = 'none';
+    }
+
+    function openPopup7() {
+        document.getElementById('popup7').style.display = 'flex';
+    }
+
+    function closePopup7() {
+        document.getElementById('popup7').style.display = 'none';
+    }
+
+    function openPopup8() {
+        document.getElementById('popup8').style.display = 'flex';
+    }
+
+    function closePopup8() {
+        document.getElementById('popup8').style.display = 'none';
+    }
+
+    function openPopup9() {
+        document.getElementById('popup9').style.display = 'flex';
+    }
+
+    function closePopup9() {
+        document.getElementById('popup9').style.display = 'none';
+    }
+
+
